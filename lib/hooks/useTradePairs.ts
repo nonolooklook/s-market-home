e29b35fb4e92862947dafcea0b800898f1d95216
nonolook <looklooknono@outlook.com>
2024-03-14 15:21:30 +0800

    use zustand

--- lib/hooks/useTradePairs.ts
+++ lib/hooks/useTradePairs.ts
@@ -1,6 +1,7 @@
-import { Address, useQuery } from 'wagmi'
-import { TradePair } from '../types'
+import { Address } from 'wagmi'
+import { create } from 'zustand'
 import { BASE_URL } from '../config'
+import { TradePair } from '../types'
 
 type TP = {
   id: number
@@ -12,28 +13,37 @@ type TP = {
   update_time: string
 }
 
-export function useTradePairs(): {
+export type UseTradePairs = {
   pairs: TradePair[]
-  loading: boolean
-} {
-  const url = `${BASE_URL}/common/order/tradingPair/list`
-  const { data = [], isLoading } = useQuery([url], ({ signal }) =>
-    fetch(url, { signal })
-      .then((res) => res.json())
-      .then((data) => data.data as TP[]),
-  )
-  return {
-    loading: isLoading,
-    pairs: data.map<TradePair>((tp) => ({
-      id: tp.id.toFixed(),
-      assetType: 'ERC1155',
-      assetId: 1n,
-      asset: tp.collection_address,
-      token: tp.token_address,
-      tokenSymbol: 'USDC',
-    })),
-  }
+  autoupdate: () => Promise<void>
 }
+const useTradePairs = create<UseTradePairs>((set) => ({
+  pairs: [],
+  autoupdate: async () => {
+    while (true) {
+      try {
+        const url = `${BASE_URL}/common/order/tradingPair/list`
+        const datas = await fetch(url)
+          .then((res) => res.json())
+          .then((data) => data.data as TP[])
+        const pairs = datas.map<TradePair>((tp) => ({
+          id: tp.id.toFixed(),
+          assetType: 'ERC1155',
+          assetId: 1n,
+          asset: tp.collection_address,
+          token: tp.token_address,
+          tokenSymbol: 'USDC',
+        }))
+        set({ pairs })
+        await new Promise((resolve) => setTimeout(resolve as any, 60 * 60 * 1000))
+      } catch (error) {
+        console.error('useTradePairs autoupdate', error)
+        await new Promise((resolve) => setTimeout(resolve as any, 5 * 1000))
+      }
+    }
+  },
+}))
+useTradePairs.getState().autoupdate()
 
 export function useGetTradePair(key?: string) {
   const { pairs } = useTradePairs()
